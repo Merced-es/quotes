2c13778ec37e95fc6320938f7442590fdb8c56d5
Mercedes <mercedes.nmx@gmail.com>
2020-06-18 15:23:06 -0600

    Started implementation of RecyclerView

--- app/src/main/java/edu/cnm/deepdive/quotes/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/viewmodel/MainViewModel.java
@@ -5,18 +5,20 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.Lifecycle.Event;
 import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
 import edu.cnm.deepdive.quotes.model.entity.Quote;
+import edu.cnm.deepdive.quotes.model.pojo.QuoteWithSource;
 import edu.cnm.deepdive.quotes.service.QuotesRepository;
 import io.reactivex.disposables.CompositeDisposable;
 import java.util.List;
 
-public class MainViewModel extends AndroidViewModel {
+public class MainViewModel extends AndroidViewModel implements LifecycleObserver {
 
   private final QuotesRepository repository;
-  private final MutableLiveData<List<Quote>> quotes;
+  private final MutableLiveData<List<QuoteWithSource>> quotes;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
 
@@ -29,7 +31,7 @@ public class MainViewModel extends AndroidViewModel {
     loadQuotes();
   }
 
-  public LiveData<List<Quote>> getQuotes() {
+  public LiveData<List<QuoteWithSource>> getQuotes() {
     return quotes;
   }
 
@@ -46,8 +48,11 @@ public class MainViewModel extends AndroidViewModel {
         )
     );
   }
+
   @OnLifecycleEvent(Event.ON_STOP)
   private void clearPending() {
+
     pending.clear();
   }
+
 }
